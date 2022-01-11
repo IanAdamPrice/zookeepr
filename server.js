901f71bff61e13d0b1a4af066958dd7685e3a3ed
IanAdamPrice <ianadamprice@gmail.com>
2022-01-11 18:17:36 -0500

    added id function

--- server.js
+++ server.js
@@ -39,7 +39,14 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
+}; // end of function
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
 }
+
+
 
 app.get('/api/animals', (req, res) => {
   let results = animals;
@@ -47,6 +54,15 @@ app.get('/api/animals', (req, res) => {
       results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
