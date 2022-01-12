98aaa24f95332af7eb26208e194619b2b1cb56a9
IanAdamPrice <ianadamprice@gmail.com>
2022-01-12 16:29:08 -0500

    post

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 const { animals } = require('./data/animals')
 
 function filterByQuery(query, animalsArray) {
@@ -44,6 +51,33 @@ function filterByQuery(query, animalsArray) {
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
+};
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
 }
 
 
@@ -65,6 +99,18 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+   req.body.id = animals.length.toString();
+
+   if (!validateAnimal(req.body)) {
+       res.status(400).send('The animal is not properly formatted.');
+   } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+   }
+});
+
 app.listen(PORT, () => {
-    console.log(`API server now on port {PORT}!`);
+    console.log(`API server now on port ${PORT}!`);
 });
